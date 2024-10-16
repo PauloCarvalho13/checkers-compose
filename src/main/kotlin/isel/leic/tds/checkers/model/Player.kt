0db245f187480bf2736e4dc49622c6f24bfd8ab5
PauloCarvalho13 <A50494@alunos.isel.pt>
2024-10-16 18:37:56 +0100

    Show Board Console implemented and Player enum improved

--- src/main/kotlin/isel/leic/tds/checkers/model/Player.kt
+++ src/main/kotlin/isel/leic/tds/checkers/model/Player.kt
@@ -2,6 +2,6 @@ package isel.leic.tds.checkers.model
 
 enum class Player {
     BLACK, WHITE;
-
+    val symbol get() = if (this == WHITE) 'w' else 'b'
     val other get() = if (this == WHITE) BLACK else WHITE
 }
--- src/main/kotlin/isel/leic/tds/checkers/ui/Output_Console.kt
+++ src/main/kotlin/isel/leic/tds/checkers/ui/Output_Console.kt
@@ -1,7 +1,45 @@
 package isel.leic.tds.checkers.ui
 
-import isel.leic.tds.checkers.model.Game
+import isel.leic.tds.checkers.model.*
 
 fun Game.showBoard(){
-    TODO()
+
+    print("  ")
+    repeat(BOARD_DIM) { col -> print("${'a' + col} ") }
+    println()
+    println(" +${"-".repeat(BOARD_DIM * 2 - 1)}+")
+
+    (BOARD_DIM downTo 1).forEach { row ->
+
+        print("$row|")
+
+        repeat(BOARD_DIM) { col ->
+            val square = Square(Row(row - 1), Column(col))
+            val boardPosition = board.find { it.square == square }
+
+            if (boardPosition?.piece != null) {
+                val piece = boardPosition.piece
+                val playerChar =
+                    if (piece.player == Player.BLACK) piece.player.symbol
+                    else piece.player.symbol
+
+                val pieceChar =
+                    if (piece.type == Type.DAMA) playerChar.uppercaseChar()
+                    else playerChar
+                print("$pieceChar ")
+            } else {
+                if (square.black) {
+                    print("- ")
+                } else {
+                    print("  ")
+                }
+            }
+        }
+        println("|")
+    }
+
+    println(" +${"-".repeat(BOARD_DIM * 2 - 1)}+")
+
+    println("Turn = ${turn.name.lowercase()}")
+    println("Player = ${turn.other.name.lowercase()}")
 }
